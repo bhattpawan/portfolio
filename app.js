fbd5939a2eed233d34fbd2da57394629e41d7e86
Pawan Bhatt <bhattpawan1997@gmail.com>
2021-04-10 21:48:57 +0530

    Final Revamp

--- app.js
+++
@@ -1,9 +0,0 @@
-document.querySelector(".hamburger").addEventListener("click", (e) => {
-  e.preventDefault();
-  let navigation = document.querySelector(".nav-list");
-  if (navigation.style.left == "-300px") {
-    navigation.style.left = "0";
-  } else {
-    navigation.style.left = "-300px";
-  }
-});
